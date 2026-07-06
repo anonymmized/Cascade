a0a4c1f0b795695f3ecc12efd68fd7e51e31a5f2
anonymmized <lih0deev@inbox.ru>
2026-07-06 21:29:09 +0300

    header file was added with new class 'Structurer'

--- structurer.cpp
+++ structurer.cpp
@@ -1,3 +1,5 @@
+#include "structurer.hpp"
+
 #include <iostream>
 #include <string>
 #include <regex>
@@ -10,25 +12,27 @@
 
 std::map<std::string, std::vector<std::string>> callGraph;
 
-std::ifstream openFile(const std::string& fileToOpen) {
-    std::ifstream openedFile(fileToOpen);
+void Structurer::setFile(const std::string& _targetFile) {
+    targetFile = _targetFile;
+}
+
+std::ifstream Structurer::openFile() {
+    std::ifstream openedFile(targetFile);
     if (!openedFile.is_open()) {
-        std::cerr << "File " << fileToOpen << "wasn't opened\n";
+        std::cerr << "File " << targetFile << "wasn't opened\n";
     }
     return openedFile;
 }
 
-std::string readCodeFromFile(const std::string& fileToRead) {
-    std::ifstream openedFile = openFile(fileToRead);
-    std::string fullCode;
+void Structurer::readCodeFromFile() {
+    std::ifstream openedFile = openFile(targetFile);
     std::string line;
     while (std::getline(openedFile, line)) {
-        fullCode += line + '\n';
+        fileCode += line + '\n';
     }
-    return fullCode;
 }
 
-std::vector<std::string> findCalls(const std::string& functionBody) {
+std::vector<std::string> Structurer::findCalls(const std::string& functionBody) {
     std::vector<std::string> functionsNames;
     std::regex regularCalls(R"((\w+)\s*\()");
     auto foundRegex = std::sregex_iterator(functionBody.begin(), functionBody.end(), regularCalls);
@@ -38,39 +42,53 @@ std::vector<std::string> findCalls(const std::string& functionBody) {
     return functionsNames;
 }
 
-size_t getBracePosition(const std::string& code, const std::string& fnName) {
+std::string Structurer::getBody(const std::string& fnName) {
+    size_t functionStart = getBracePosition(fnName);
+    int depth = 0;
+    size_t i = functionStart;
+    for (; i < fileCode.size(); i++) {
+        if (fileCode[i] == '{') {
+            depth += 1;
+        } else if (fileCode[i] == '}' && --depth == 0) {
+            break;
+        }
+    }
+
+    std::string functionBody = fileCode.substr(functionStart, i - functionStart + 1);
+    return functionBody;
+}
+
+size_t Structurer::getBracePosition(const std::string& fnName) {
     std::regex re(R"(\b)" + fnName + R"(\s*\([^)]*\)\s*\{)");
     std::smatch match;
-    if (!std::regex_search(code, match, re)) {
+    if (!std::regex_search(fileCode, match, re)) {
         return std::string::npos;
     }
     return match.position(0) + match.length(0) - 1;
 }
 
-std::string getBody(const std::string& code, const std::string& fnName) {
-    size_t functionStart = getBracePosition(code, fnName);
-    int depth = 0;
-    size_t i = functionStart;
-    for (; i < code.size(); i++) {
-        if (code[i] == '{') {
-            depth += 1;
-        } else if (code[i] == '}' && --depth == 0) {
-            break;
-        }
+void Structurer::addToGraph(const std::string& fnName) {
+    std::string body = getBody(fnName);
+    std::vector<std::string> calls = findCalls(body);
+    callGraph.push_back(fnName);
+    callGraph[fnName] = calls;
+}
+
+void setMainBraceAndName() {
+    std::regex re(R"(//\s*MAIN\s*[\r\n]+[^\n(]*?(\w+)\s*\()");
+    std::smatch match;
+    if (std::regex_search(fileCode, match, re)) {
+        mainName = match[1];
     }
-    std::string functionBody = code.substr(functionStart, i - functionStart + 1);
-    return functionBody;
+    size_t functionStart = match.position(1) + match.length(0);
+    mainBracePos = fileCode.find('{', functionStart);
 }
 
 int main() {
-    std::string fullCode = readCodeFromFile("testCode.cpp");
-    std::regex reMain(R"(//\s*MAIN\s*[\r\n]+[^\n(]*?(\w+)\s*\()");
-    std::smatch functionNameMatch;
-    if (std::regex_search(fullCode, functionNameMatch, reMain)) {
-        std::cout << "Main function name: " << functionNameMatch[1] << '\n';
-    }
-    size_t functionStart = functionNameMatch.position(1) + functionNameMatch.length(0);
-    size_t whereBraceOpens = fullCode.find('{', functionStart);
+    Structurer structer;
+    structer.setFile("./testCode.cpp");
+    structer.readCodeFromFile();
+
     int depth = 0;
     size_t i = whereBraceOpens;
     for (; i < fullCode.size(); i++) {
@@ -80,14 +98,9 @@ int main() {
             break;
         }
     }
-    std::string functionBody = fullCode.substr(functionStart, i - functionStart + 1);
-    std::cout << "Function body:\n" << functionBody << '\n';
-    std::vector<std::string> mainCalls = findCalls(functionBody);
-    callGraph[functionNameMatch[1]] = mainCalls;
-    for (const auto& call : mainCalls) {
-        std::string name = call;
-        std::string body = getBody(fullCode, name);
-        callGraph[name] = findCalls(body);
+    structer.addToGraph(mainName);
+    for (const auto& fnName : mainCalls[0]) {
+        structer.addToGraph(fnName);
     }
     for (const auto& [key, value] : callGraph) {
         std::cout << key << ":\n";
---
+++ structurer.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+#include <utility>
+#include <vector>
+#include <fstream>
+
+class Structurer {
+    private:
+        std::vector<std::pair<std::string, std::vector<std::string>>> callGraph;
+        std::string targetFile;
+        std::string fileCode;
+        size_t mainBracePos;
+        std::string mainName;
+    public:
+        Structurer(const std::string& _targetFile) : targetFile(_targetFile) {}
+        Structurer() = default;
+        void setFile(const std::string _targetFile);
+        std::ifstream openFile();
+        void readCodeFromFile();
+        std::vector<std::string> findCalls(const std::string& functionBody);
+        std::string getBody(const std::string& fnName);
+        size_t getBracePosition(const std::string& fnName);
+        void addToGraph(const std::string fnName);
+        void setMainBraceAndName();
+};
